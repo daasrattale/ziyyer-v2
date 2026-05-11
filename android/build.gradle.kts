bf022035aca9dfc27f6e9e19783969dd9f26a963
ELATTAR Saad <sa.elattar.ad@gmail.com>
2026-05-11 23:52:11 +0200

    fix: isar android build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,9 +12,17 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+                if (namespace == null) {
+                    namespace = group.toString()
+                }
+            }
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
--- lib/database/base_repository.dart
+++ lib/database/base_repository.dart
@@ -1,4 +1,4 @@
-import 'package:isar/isar.dart';
+import 'package:isar_community/isar.dart';
 
 /// Generic CRUD operations for Isar collections
 ///

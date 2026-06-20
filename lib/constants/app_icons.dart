c452fc078de72281ece2354f3323e41ed958b2b9
ELATTAR Saad <sa.elattar.ad@gmail.com>
2026-06-21 01:48:03 +0200

    add job to publish release apk

--- lib/constants/app_icons.dart
+++ lib/constants/app_icons.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:feather_icons/feather_icons.dart';
 
 class AppIcons {
   AppIcons._();
 
-  static const IconData home = FontAwesomeIcons.house;
+  static const IconData home = FeatherIcons.home;
   static const IconData history = Icons.history;
-  static const IconData insights = FontAwesomeIcons.chartPie;
+  static const IconData insights = FeatherIcons.pieChart;
   static const IconData wallet = Icons.wallet;
-  static const IconData notification = FontAwesomeIcons.bell;
-  static const IconData add = FontAwesomeIcons.plus;
-  static const IconData coffee = FontAwesomeIcons.coffee;
-  static const IconData truck = FontAwesomeIcons.truck;
-  static const IconData shoppingBag = FontAwesomeIcons.shoppingBag;
-  static const IconData film = FontAwesomeIcons.film;
-  static const IconData trendingUp = FontAwesomeIcons.arrowTrendUp;
-  static const IconData trendingDown = FontAwesomeIcons.arrowTrendDown;
-  static const IconData arrowUpRight = FontAwesomeIcons.arrowUp;
-  static const IconData arrowDownRight = FontAwesomeIcons.arrowDown;
+  static const IconData notification = FeatherIcons.bell;
+  static const IconData add = FeatherIcons.plus;
+  static const IconData coffee = FeatherIcons.coffee;
+  static const IconData truck = FeatherIcons.truck;
+  static const IconData shoppingBag = FeatherIcons.shoppingBag;
+  static const IconData film = FeatherIcons.film;
+  static const IconData trendingUp = FeatherIcons.trendingUp;
+  static const IconData trendingDown = FeatherIcons.trendingDown;
+  static const IconData arrowUpRight = FeatherIcons.arrowUpRight;
+  static const IconData arrowDownRight = FeatherIcons.arrowDownRight;
 }
